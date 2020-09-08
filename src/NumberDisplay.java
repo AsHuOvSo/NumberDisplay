a4582e3ad9cf1ea0e511a848c9b5126dd1ca10e2
AsHuOvSo <asta@avra.dk>
2020-09-08 11:46:36 +0200

    Initial commit

---
+++ src/NumberDisplay.java
@@ -0,0 +1,18 @@
+public class NumberDisplay {
+    //starting program
+    public static void main(String[] args) {
+        //printing lines for display of numbers
+        System.out.println("-------------------------");
+        System.out.println("|  a  | a^2 | a^3 | a^4 |");
+        System.out.println("|-----------------------|");
+        System.out.println("|  1  |  1  |  1  |  1  |");
+        System.out.println("|-----------------------|");
+        System.out.println("|  2  |  4  |  8  |  16 |");
+        System.out.println("|-----------------------|");
+        System.out.println("|  3  |  9  |  27 |  81 |");
+        System.out.println("|-----------------------|");
+        System.out.println("|  4  |  16 |  64 | 256 |");
+        System.out.println("-------------------------");
+
+    }
+}
